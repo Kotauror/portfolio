75de8f2bdab2930f9c63aad27015ed7d7a7ae3a1
kotpsot <zygmunt.justyna@gmail.com>
2018-03-14 19:03:34 +0000

    boris and oyster ready

--- interfaces/navbar_interface.js
+++ interfaces/navbar_interface.js
@@ -4,6 +4,10 @@ $( document ).ready(function() {
     $(".description").load('projects/boris.html');
   });
 
+  $(".oyster").click(function(){
+    $(".description").load('projects/oystercard.html');
+  });
+
   $(".navbar-brand").click(function(){
     $(".description").text('thank you!');
   });
