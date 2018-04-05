15d871e8308f76e8251cf5f3948f3751f6366b8f
kotpsot <zygmunt.justyna@gmail.com>
2018-04-05 20:17:05 +0100

    Mobile-friendly.

--- interfaces/about_me_interface.js
+++ interfaces/about_me_interface.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
   $(".back-end").click(function(){
-    $(".back-end").text('Ruby, Rails, Javascript, Node.js, Sequelize');
+    $(".back-end").text('Ruby, Rails, Node.js, Sequelize');
   });
 
   $(".databases").click(function(){
@@ -9,7 +9,7 @@ $( document ).ready(function() {
   });
 
   $(".front-end").click(function(){
-    $(".front-end").text('HTML, CSS, Javascript, Bootstrap, EaselJS');
+    $(".front-end").text('HTML, CSS, Javascript, jQuery, Bootstrap, EaselJS');
   });
 
   $(".hosting").click(function(){
@@ -19,5 +19,18 @@ $( document ).ready(function() {
   $(".testing").click(function(){
     $(".testing").text('RSpec, Jasmine, Mocha, Zombie, Capybara ');
   });
+
+  $(".agile").click(function(){
+    $(".agile").text('In my previous job as a lawyer I used to be responsible for drafting software development contracts suited for agile working. This helps me a lot to work as an software developer, as I fully understand what is expected of an agile team.');
+  });
+
+  $(".tdd").click(function(){
+    $(".tdd").text("I'm sticking to TDD as much as possible, what helps me to produce a fully-working, clean and easy to read code. In a vast majority of cases, my test coverate is > 95%.");
+  });
+
+  $(".solid").click(function(){
+    $(".solid").text("Everyday I learn more about practical implementations of SOLID principles, that are quickly becoming cornerstones of my projects, expecially the separation of concerns and keeping the code open for extensions.");
+  });
+
 
 })
